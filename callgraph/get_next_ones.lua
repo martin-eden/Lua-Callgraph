014cb5b3783c13193ff7b30f5f33f78debf9d72e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-15 17:48:44 +0200

    [^]

---
+++ callgraph/get_next_ones.lua
@@ -0,0 +1,138 @@
+-- Return table with possible next instructions indices
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-07-15
+]]
+
+-- Core of this ship
+
+local FlowOpcodes =
+  {
+    return_nothing = 'RETURN0',
+    return_item = 'RETURN1',
+    return_sequence = 'RETURN',
+
+    jump = 'JMP',
+
+    equal_reg = 'EQ',
+    equal_const_table = 'EQK',
+    less_than = 'LT',
+    less_or_equal = 'LE',
+
+    equal_imm = 'EQI',
+    less_than_imm = 'LTI',
+    less_or_equal_imm = 'LEI',
+    greater_than_imm = 'GTI',
+    greater_or_equal_imm = 'GEI',
+
+    set_false_and_skip = 'LFALSESKIP',
+
+    if_neq_then_skip = 'TEST',
+    if_neq_then_skip_else_set = 'TESTSET',
+
+    check_numeric_loop = 'FORPREP',
+    check_generic_loop = 'TFORPREP',
+
+    numeric_loop_back = 'FORLOOP',
+    generic_loop_back = 'TFORLOOP',
+  }
+
+local Terminators =
+  {
+    FlowOpcodes.return_nothing,
+    FlowOpcodes.return_item,
+    FlowOpcodes.return_sequence,
+  }
+
+local Directors =
+  {
+    FlowOpcodes.jump,
+  }
+
+local Skippers =
+  {
+    FlowOpcodes.equal_reg,
+    FlowOpcodes.equal_const_table,
+    FlowOpcodes.less_than,
+    FlowOpcodes.less_or_equal,
+    FlowOpcodes.equal_imm,
+    FlowOpcodes.less_than_imm,
+    FlowOpcodes.less_or_equal_imm,
+    FlowOpcodes.greater_than_imm,
+    FlowOpcodes.greater_or_equal_imm,
+    FlowOpcodes.set_false_and_skip,
+    FlowOpcodes.if_neq_then_skip,
+    FlowOpcodes.if_neq_then_skip_else_set,
+  }
+
+local ForwardJumpers =
+  {
+    FlowOpcodes.check_numeric_loop,
+    FlowOpcodes.check_generic_loop,
+  }
+
+local BackwardJumpers =
+  {
+    FlowOpcodes.numeric_loop_back,
+    FlowOpcodes.generic_loop_back,
+  }
+
+local get_next_ones
+do
+  local Terminators_Map
+  local Directors_Map
+  local Skippers_Map
+  local ForwardJumpers_Map
+  local BackwardJumpers_Map
+  do
+    local map_values = request('!.table.map_values')
+
+    Terminators_Map = map_values(Terminators)
+    Directors_Map = map_values(Directors)
+    Skippers_Map = map_values(Skippers)
+    ForwardJumpers_Map = map_values(ForwardJumpers)
+    BackwardJumpers_Map = map_values(BackwardJumpers)
+  end
+
+  local add_to_list = request('!.concepts.list.add_item')
+
+  get_next_ones =
+    function(instruction_index, Instruction)
+      local NextOnes = { }
+
+      local opcode = Instruction[1]
+      local next_instruction_index = instruction_index +  1
+
+      if Terminators_Map[opcode] then
+        ;
+      elseif Directors_Map[opcode] then
+        local jump_offset = tonumber(Instruction[2])
+
+        add_to_list(NextOnes, next_instruction_index + jump_offset)
+      elseif Skippers_Map[opcode] then
+        add_to_list(NextOnes, next_instruction_index)
+        add_to_list(NextOnes, next_instruction_index + 1)
+      elseif ForwardJumpers_Map[opcode] then
+        local jump_offset = tonumber(Instruction[3])
+
+        add_to_list(NextOnes, next_instruction_index + jump_offset)
+      elseif BackwardJumpers_Map[opcode] then
+        local jump_offset = tonumber(Instruction[3])
+
+        add_to_list(NextOnes, next_instruction_index)
+        add_to_list(NextOnes, next_instruction_index - jump_offset)
+      else
+        add_to_list(NextOnes, next_instruction_index)
+      end
+
+      return NextOnes
+    end
+end
+
+-- Export:
+return get_next_ones
+
+--[[
+  2026-07-15
+]]
--- run.lua
+++ run.lua
@@ -8,94 +8,29 @@
 package.path = package.path .. ';../../?.lua'
 require('workshop.base')
 
-local get_functions =
-  function(func)
-    local Functions
+local process_function
+do
+  local get_next_ones = request('callgraph.get_next_ones')
+  local list_to_str = request('!.concepts.list.to_string')
+  local add_to_list = request('!.concepts.list.add_item')
 
-    local get_functions = request('!.concepts.lua_bytecode_decompiler.parse')
-
-    Functions = get_functions(func)
-
-    return Functions
-  end
-
-local create_instruction_rec =
-  function()
-    return
-      {
-        label = '',
-        NextOnes = { },
-      }
-  end
-
-local process_function =
-  function(Function)
-    local Result = { }
-
-    local add_to_list = request('!.concepts.list.add_item')
-
-    for instruction_index, Instruction in ipairs(Function) do
-      add_to_list(Result, create_instruction_rec())
-    end
-
-    local list_to_str = request('!.concepts.list.to_string')
-
-    for instruction_index, Instruction in ipairs(Function) do
-      local instruction_str = list_to_str(Instruction, ' ')
-
-      local ResultRec = Result[instruction_index]
-
-      ResultRec.label = instruction_str
-    end
-
-    do
-      local opcode_test = 'TEST'
-      local opcode_less = 'LT'
-      local opcode_less_eq = 'LE'
-      local opcode_jump = 'JMP'
-      local opcode_equal_reg = 'EQ'
-      local opcode_equal_const = 'EQI'
-      local opcode_forloop = 'TFORLOOP'
-
-      local num_instructions = #Function
+  process_function =
+    function(Function)
+      local Callgraph = { }
 
       for instruction_index, Instruction in ipairs(Function) do
-        local NextOnes = Result[instruction_index].NextOnes
+        local CallgraphRec =
+          {
+            label = list_to_str(Instruction, ' '),
+            NextOnes = get_next_ones(instruction_index, Instruction),
+          }
 
-        local is_last_one = (instruction_index == num_instructions)
-
-        local opcode = Instruction[1]
-
-        local next_instruction_index = instruction_index +  1
-
-        if
-          (opcode == opcode_test) or
-          (opcode == opcode_less) or
-          (opcode == opcode_less_eq) or
-          (opcode == opcode_equal_reg) or
-          (opcode == opcode_equal_const)
-        then
-          add_to_list(NextOnes, next_instruction_index)
-          add_to_list(NextOnes, next_instruction_index + 1)
-        elseif (opcode == opcode_jump) then
-          local jump_offset = tonumber(Instruction[2])
-
-          add_to_list(NextOnes, next_instruction_index + jump_offset)
-        elseif (opcode == opcode_forloop) then
-          local jump_offset = tonumber(Instruction[3])
-
-          add_to_list(NextOnes, next_instruction_index)
-          add_to_list(NextOnes, next_instruction_index - jump_offset)
-        else
-          if not is_last_one then
-            add_to_list(NextOnes, next_instruction_index)
-          end
-        end
+        add_to_list(Callgraph, CallgraphRec)
       end
-    end
 
-    return Result
-  end
+      return Callgraph
+    end
+end
 
 local export_to_tgf
 local export_to_dot
@@ -151,7 +86,7 @@ local print_help =
 
 -- Main:
 do
-  -- local t2s = request('!.convert.value_to_str')
+  local t2s = request('!.convert.value_to_str')
 
   local source_code_str
   do
@@ -173,7 +108,6 @@ do
     request('!.concepts.lua_bytecode_decompiler.listing_from_bytecode')
 
   local bytecode = get_bytecode(source_code_str)
-
   local Functions = get_listing(bytecode)
 
   -- print(t2s(Functions))
