93e8992cd4c12f1f071ebaae201a3eaf4e534dd6
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-23 17:14:40 +0200

    [.^] [callgraph_to_tgf] Code composition.

--- src/callgraph/callgraph_to_tgf.lua
+++ src/callgraph/callgraph_to_tgf.lua
@@ -5,102 +5,107 @@
   Last mod.: 2026-07-23
 ]]
 
-local OutputStream
-
-local write =
-  function(str)
-    OutputStream:Write(str)
-  end
-
-local node_name_format
-
-local set_node_name_format
+local callgraph_to_tgf
 do
-  local get_num_digits = request('!.number.get_num_dec_digits')
-  local int_to_str = tostring
+  local OutputStream
 
-  set_node_name_format =
-    function(num_instructions)
-      local num_digits = get_num_digits(num_instructions)
-      node_name_format = '%' .. '0' .. int_to_str(num_digits) .. 'd'
+  local write =
+    function(str)
+      OutputStream:Write(str)
     end
-end
 
-local get_node_name
-do
-  local str_format = string.format
+  local space = ' '
+  local newline = '\010'
 
-  get_node_name =
-    function(index)
-      return str_format(node_name_format, index)
-    end
-end
-
-local space = ' '
-local newline = '\010'
-
-local write_label =
-  function(name, label)
-    write(name)
-    write(space)
-    write(label)
-    write(newline)
-  end
-
-local write_sections_delimiter
-do
   local parts_delim = '#'
 
-  write_sections_delimiter =
+  local write_label =
+    function(name, label)
+      write(name)
+      write(space)
+
+      write(label)
+      write(newline)
+    end
+
+  local write_sections_delimiter =
     function()
       write(newline)
+
       write(parts_delim)
       write(newline)
+
       write(newline)
     end
-end
 
-local write_link =
-  function(src_name, dest_name)
-    write(src_name)
-    write(space)
-    write(dest_name)
-    write(newline)
-  end
+  local write_link =
+    function(src_name, dest_name)
+      write(src_name)
+      write(space)
 
-local callgraph_to_tgf =
-  function(InstructionsGraph, Arg_OutputStream)
-    OutputStream = Arg_OutputStream
+      write(dest_name)
+      write(newline)
+    end
+
+  local set_node_name_format
+  local get_node_name
+  do
+    local node_name_format
 
     do
-      local num_instructions = #InstructionsGraph
-      set_node_name_format(num_instructions)
+      local get_num_digits = request('!.number.get_num_dec_digits')
+      local int_to_str = tostring
+
+      set_node_name_format =
+        function(num_instructions)
+          local num_digits = get_num_digits(num_instructions)
+          node_name_format = '%0' .. int_to_str(num_digits) .. 'd'
+        end
     end
+    do
+      local str_format = string.format
 
-    for instruction_index, Instruction in ipairs(InstructionsGraph) do
-      local name = get_node_name(instruction_index)
-      write_label(name, Instruction.label)
-    end
-
-    write_sections_delimiter()
-
-    for src_instruction_index, Instruction in ipairs(InstructionsGraph) do
-      -- Implementation surrounds branch nodes with empty lines
-
-      local NextOnes = Instruction.NextOnes
-      local is_branch_node = (#NextOnes > 1)
-
-      if is_branch_node then write(newline) end
-
-      for _, dest_instruction_index in ipairs(NextOnes) do
-        local src_name = get_node_name(src_instruction_index)
-        local dest_name = get_node_name(dest_instruction_index)
-        write_link(src_name, dest_name)
-      end
-
-      if is_branch_node then write(newline) end
+      get_node_name =
+        function(index)
+          return str_format(node_name_format, index)
+        end
     end
   end
+
+  callgraph_to_tgf =
+    function(InstructionsGraph, Arg_OutputStream)
+      OutputStream = Arg_OutputStream
+
+      do
+        local num_instructions = #InstructionsGraph
+        set_node_name_format(num_instructions)
+      end
+
+      for instruction_index, Instruction in ipairs(InstructionsGraph) do
+        local name = get_node_name(instruction_index)
+        write_label(name, Instruction.label)
+      end
+
+      write_sections_delimiter()
+
+      for src_instruction_index, Instruction in ipairs(InstructionsGraph) do
+        -- Implementation surrounds branch nodes with empty lines
+
+        local NextOnes = Instruction.NextOnes
+        local is_branch_node = (#NextOnes > 1)
+
+        if is_branch_node then write(newline) end
+
+        for _, dest_instruction_index in ipairs(NextOnes) do
+          local src_name = get_node_name(src_instruction_index)
+          local dest_name = get_node_name(dest_instruction_index)
+          write_link(src_name, dest_name)
+        end
+
+        if is_branch_node then write(newline) end
+      end
+    end
+end
 
 -- Export:
 return callgraph_to_tgf
@@ -108,4 +113,5 @@ return callgraph_to_tgf
 --[[
   2026-07-15
   2026-07-17
+  2026-07-23
 ]]
