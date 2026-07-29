6b661d60a8af45e0f01d252ff05a93c7f8b9935e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-29 22:32:56 +0200

    [^+?] [get_next_ones] Unfinished adjustments for Lua 5.3.

--- src/callgraph/get_next_ones.lua
+++ src/callgraph/get_next_ones.lua
@@ -2,12 +2,26 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-07-28
+  Last mod.: 2026-07-29
 ]]
 
 -- Core of this ship
 
--- Implemented for Lua 5.5 opcodes and their semantics
+--[[
+  Checked for Lua versions from 5.3 to 5.5.
+
+  ..
+
+  After good bath thinking we decided to diverge this module into
+  two instances. Because VM in 5.4 changed logic of existing
+  instruction (and added moar instructionz).
+
+  Each instance will start with knowledge about flow-related
+  instructions for their VM.
+
+  Each instance must return result that complies with common
+  interface (aka function "get_next_one(bla, bla)").
+]]
 
 local get_next_ones
 do
@@ -19,9 +33,9 @@ do
   local SkippersAndBackwardJumpers_Map
   do
     local Terminators
+    local Skippers
     local Jumpers
     local ForwardJumpers
-    local Skippers
     local SkippersAndForwardJumpers
     local SkippersAndBackwardJumpers
     do
@@ -32,16 +46,6 @@ do
           FlowOpcodes.return_nothing,
           FlowOpcodes.return_item,
           FlowOpcodes.return_sequence,
-        }
-
-      Jumpers =
-        {
-          FlowOpcodes.jump,
-        }
-
-      ForwardJumpers =
-        {
-          FlowOpcodes.check_generic_loop,
         }
 
       Skippers =
@@ -60,6 +64,16 @@ do
           FlowOpcodes.if_neq_then_skip_else_set,
         }
 
+      Jumpers =
+        {
+          FlowOpcodes.jump,
+        }
+
+      ForwardJumpers =
+        {
+          FlowOpcodes.check_generic_loop,
+        }
+
       SkippersAndForwardJumpers =
         {
           FlowOpcodes.check_numeric_loop,
@@ -75,39 +89,58 @@ do
     local map_values = request('!.table.map_values')
 
     Terminators_Map = map_values(Terminators)
+    Skippers_Map = map_values(Skippers)
     Jumpers_Map = map_values(Jumpers)
     ForwardJumpers_Map = map_values(ForwardJumpers)
-    Skippers_Map = map_values(Skippers)
     SkippersAndForwardJumpers_Map = map_values(SkippersAndForwardJumpers)
     SkippersAndBackwardJumpers_Map = map_values(SkippersAndBackwardJumpers)
   end
 
   local add_to_list = request('!.concepts.list.add_item')
 
+  local jump_offset_is_signed
+  local unused_op_in_jump
+  do
+    local is_ancient_lua =
+      (_G._VERSION ~= 'Lua 5.4') and
+      (_G._VERSION ~= 'Lua 5.5')
+    jump_offset_is_signed = is_ancient_lua
+    unused_op_in_jump = is_ancient_lua
+  end
+
   get_next_ones =
     function(instruction_index, Instruction)
       local NextOnes = { }
 
       local opcode = Instruction[1]
-      local next_instruction_index = instruction_index +  1
+      local next_instruction_index = instruction_index + 1
 
       if Terminators_Map[opcode] then
         ;
+      elseif Skippers_Map[opcode] then
+        add_to_list(NextOnes, next_instruction_index)
+        add_to_list(NextOnes, next_instruction_index + 1)
       elseif Jumpers_Map[opcode] then
-        local jump_offset = tonumber(Instruction[2])
+        local jump_offset
+        if unused_op_in_jump then
+          jump_offset = Instruction[3]
+        else
+          jump_offset = Instruction[2]
+        end
+        jump_offset = tonumber(jump_offset)
         add_to_list(NextOnes, next_instruction_index + jump_offset)
       elseif ForwardJumpers_Map[opcode] then
         local jump_offset = tonumber(Instruction[3])
         add_to_list(NextOnes, next_instruction_index + jump_offset)
-      elseif Skippers_Map[opcode] then
-        add_to_list(NextOnes, next_instruction_index)
-        add_to_list(NextOnes, next_instruction_index + 1)
       elseif SkippersAndForwardJumpers_Map[opcode] then
         local jump_offset = tonumber(Instruction[3])
         add_to_list(NextOnes, next_instruction_index)
         add_to_list(NextOnes, next_instruction_index + jump_offset)
       elseif SkippersAndBackwardJumpers_Map[opcode] then
         local jump_offset = tonumber(Instruction[3])
+        if jump_offset_is_signed then
+          jump_offset = -jump_offset
+        end
         add_to_list(NextOnes, next_instruction_index - jump_offset)
         add_to_list(NextOnes, next_instruction_index)
       else
@@ -123,4 +156,5 @@ return get_next_ones
 
 --[[
   2026-07-15
+  2026-07-29
 ]]
