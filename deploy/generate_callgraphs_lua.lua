84c39d6dce1b002c08d15b5396e1887e49f6e43e
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-07-30 19:15:58 +0200

    [~] [deploy]

--- deploy/generate_callgraphs_lua.lua
+++ deploy/generate_callgraphs_lua.lua
@@ -1433,139 +1433,24 @@ _G.package.preload['workshop.concepts.path_name.add_dir_postfix'] =
       end
     return add_dir_postfix
   end
-_G.package.preload['callgraph.FlowOpcodes'] =
-  function(...)
-    local FlowOpcodes =
-      {
-        return_nothing = 'RETURN0',
-        return_item = 'RETURN1',
-        return_sequence = 'RETURN',
-        jump = 'JMP',
-        equal_reg = 'EQ',
-        equal_const_table = 'EQK',
-        less_than = 'LT',
-        less_or_equal = 'LE',
-        equal_imm = 'EQI',
-        less_than_imm = 'LTI',
-        less_or_equal_imm = 'LEI',
-        greater_than_imm = 'GTI',
-        greater_or_equal_imm = 'GEI',
-        set_false_and_skip = 'LFALSESKIP',
-        if_neq_then_skip = 'TEST',
-        if_neq_then_skip_else_set = 'TESTSET',
-        check_numeric_loop = 'FORPREP',
-        check_generic_loop = 'TFORPREP',
-        numeric_loop_back = 'FORLOOP',
-        generic_loop_back = 'TFORLOOP',
-      }
-    return FlowOpcodes
-  end
 _G.package.preload['callgraph.get_next_ones'] =
   function(...)
     local get_next_ones
     do
-      local Terminators_Map
-      local Jumpers_Map
-      local ForwardJumpers_Map
-      local Skippers_Map
-      local SkippersAndForwardJumpers_Map
-      local SkippersAndBackwardJumpers_Map
+      local use_vm_2015
+      local use_vm_2020
       do
-        local Terminators
-        local Skippers
-        local Jumpers
-        local ForwardJumpers
-        local SkippersAndForwardJumpers
-        local SkippersAndBackwardJumpers
-        do
-          local FlowOpcodes = request('FlowOpcodes')
-          Terminators =
-            {
-              FlowOpcodes.return_nothing,
-              FlowOpcodes.return_item,
-              FlowOpcodes.return_sequence,
-            }
-          Skippers =
-            {
-              FlowOpcodes.equal_reg,
-              FlowOpcodes.equal_const_table,
-              FlowOpcodes.less_than,
-              FlowOpcodes.less_or_equal,
-              FlowOpcodes.equal_imm,
-              FlowOpcodes.less_than_imm,
-              FlowOpcodes.less_or_equal_imm,
-              FlowOpcodes.greater_than_imm,
-              FlowOpcodes.greater_or_equal_imm,
-              FlowOpcodes.set_false_and_skip,
-              FlowOpcodes.if_neq_then_skip,
-              FlowOpcodes.if_neq_then_skip_else_set,
-            }
-          Jumpers = { FlowOpcodes.jump }
-          ForwardJumpers = { FlowOpcodes.check_generic_loop }
-          SkippersAndForwardJumpers = { FlowOpcodes.check_numeric_loop }
-          SkippersAndBackwardJumpers =
-            {
-              FlowOpcodes.numeric_loop_back,
-              FlowOpcodes.generic_loop_back,
-            }
-        end
-        local map_values = request('!.table.map_values')
-        Terminators_Map = map_values(Terminators)
-        Skippers_Map = map_values(Skippers)
-        Jumpers_Map = map_values(Jumpers)
-        ForwardJumpers_Map = map_values(ForwardJumpers)
-        SkippersAndForwardJumpers_Map =
-          map_values(SkippersAndForwardJumpers)
-        SkippersAndBackwardJumpers_Map =
-          map_values(SkippersAndBackwardJumpers)
+        local is_lua_53 = (_VERSION == 'Lua 5.3')
+        local is_lua_54 = (_VERSION == 'Lua 5.4')
+        local is_lua_55 = (_VERSION == 'Lua 5.5')
+        use_vm_2015 = is_lua_53
+        use_vm_2020 = is_lua_54 or is_lua_55
       end
-      local add_to_list = request('!.concepts.list.add_item')
-      local jump_offset_is_signed
-      local unused_op_in_jump
-      do
-        local is_ancient_lua =
-          (_G._VERSION ~= 'Lua 5.4') and (_G._VERSION ~= 'Lua 5.5')
-        jump_offset_is_signed = is_ancient_lua
-        unused_op_in_jump = is_ancient_lua
+      if use_vm_2015 then
+        get_next_ones = request('vm_2015.get_next_ones')
+      elseif use_vm_2020 then
+        get_next_ones = request('vm_2020.get_next_ones')
       end
-      get_next_ones =
-        function(instruction_index, Instruction)
-          local NextOnes = {}
-          local opcode = Instruction[1]
-          local next_instruction_index = instruction_index + 1
-          if Terminators_Map[opcode] then
-            ;
-          elseif Skippers_Map[opcode] then
-            add_to_list(NextOnes, next_instruction_index)
-            add_to_list(NextOnes, next_instruction_index + 1)
-          elseif Jumpers_Map[opcode] then
-            local jump_offset
-            if unused_op_in_jump then
-              jump_offset = Instruction[3]
-            else
-              jump_offset = Instruction[2]
-            end
-            jump_offset = tonumber(jump_offset)
-            add_to_list(NextOnes, next_instruction_index + jump_offset)
-          elseif ForwardJumpers_Map[opcode] then
-            local jump_offset = tonumber(Instruction[3])
-            add_to_list(NextOnes, next_instruction_index + jump_offset)
-          elseif SkippersAndForwardJumpers_Map[opcode] then
-            local jump_offset = tonumber(Instruction[3])
-            add_to_list(NextOnes, next_instruction_index)
-            add_to_list(NextOnes, next_instruction_index + jump_offset)
-          elseif SkippersAndBackwardJumpers_Map[opcode] then
-            local jump_offset = tonumber(Instruction[3])
-            if jump_offset_is_signed then
-              jump_offset = -jump_offset
-            end
-            add_to_list(NextOnes, next_instruction_index - jump_offset)
-            add_to_list(NextOnes, next_instruction_index)
-          else
-            add_to_list(NextOnes, next_instruction_index)
-          end
-          return NextOnes
-        end
     end
     return get_next_ones
   end
@@ -1714,6 +1599,119 @@ _G.package.preload['callgraph.callgraph_to_dot'] =
       end
     return callgraph_to_dot
   end
+_G.package.preload['callgraph.vm_2020.FlowOpcodes'] =
+  function(...)
+    local FlowOpcodes =
+      {
+        [1] = { 'TAILCALL', 'RETURN', 'RETURN0', 'RETURN1' },
+        [2] = 'LFALSESKIP',
+        [3] = 'JMP',
+        [4] = 'TFORPREP',
+        [5] =
+          {
+            'ADDI',
+            'ADDK',
+            'SUBK',
+            'MULK',
+            'MODK',
+            'POWK',
+            'DIVK',
+            'IDIVK',
+            'BANDK',
+            'BORK',
+            'BXORK',
+            'SHRI',
+            'SHLI',
+            'ADD',
+            'SUB',
+            'MUL',
+            'MOD',
+            'POW',
+            'DIV',
+            'IDIV',
+            'BAND',
+            'BOR',
+            'BXOR',
+            'SHL',
+            'SHR',
+            'EQ',
+            'LT',
+            'LE',
+            'EQK',
+            'EQI',
+            'LTI',
+            'LEI',
+            'GTI',
+            'GEI',
+            'TEST',
+            'TESTSET',
+          },
+        [6] = { 'FORLOOP', 'TFORLOOP' },
+        [7] = 'FORPREP',
+      }
+    return FlowOpcodes
+  end
+_G.package.preload['callgraph.vm_2020.get_next_ones'] =
+  function(...)
+    local get_next_ones
+    do
+      local Terminators_Map
+      local opcode_lfalseskip
+      local opcode_jmp
+      local opcode_tforprep
+      local BasicForks_Map
+      local Loopbacks_Map
+      local opcode_forprep
+      do
+        local FlowOpcodes = request('FlowOpcodes')
+        local map_values = request('!.table.map_values')
+        Terminators_Map = map_values(FlowOpcodes[1])
+        opcode_lfalseskip = FlowOpcodes[2]
+        opcode_jmp = FlowOpcodes[3]
+        opcode_tforprep = FlowOpcodes[4]
+        BasicForks_Map = map_values(FlowOpcodes[5])
+        Loopbacks_Map = map_values(FlowOpcodes[6])
+        opcode_forprep = FlowOpcodes[7]
+      end
+      local add_to_list = request('!.concepts.list.add_item')
+      get_next_ones =
+        function(instruction_index, Instruction)
+          local NextOnes = {}
+          local opcode = Instruction[1]
+          local next_instruction = instruction_index + 1
+          if Terminators_Map[opcode] then
+            ;
+          elseif (opcode == opcode_lfalseskip) then
+            add_to_list(NextOnes, next_instruction + 1)
+          elseif (opcode == opcode_jmp) then
+            add_to_list(
+              NextOnes, next_instruction + tonumber(Instruction[2])
+            )
+          elseif (opcode == opcode_tforprep) then
+            add_to_list(
+              NextOnes, next_instruction + tonumber(Instruction[3])
+            )
+          elseif BasicForks_Map[opcode] then
+            add_to_list(NextOnes, next_instruction)
+            add_to_list(NextOnes, next_instruction + 1)
+          elseif Loopbacks_Map[opcode] then
+            add_to_list(
+              NextOnes, next_instruction - tonumber(Instruction[3])
+            )
+            add_to_list(NextOnes, next_instruction)
+          elseif (opcode == opcode_forprep) then
+            add_to_list(NextOnes, next_instruction)
+            add_to_list(
+              NextOnes, next_instruction + tonumber(Instruction[3]) + 1
+            )
+          else
+            add_to_list(NextOnes, next_instruction)
+          end
+          return NextOnes
+        end
+    end
+    return get_next_ones
+  end
 _G.package.preload['callgraph.concepts.Ascii'] =
   function(...)
     local Ascii =
@@ -1731,6 +1729,68 @@ _G.package.preload['callgraph.concepts.Ascii'] =
         closing_bracket = ']',
       }
     return Ascii
+  end
+_G.package.preload['callgraph.vm_2015.FlowOpcodes'] =
+  function(...)
+    local FlowOpcodes =
+      {
+        [1] = { 'TAILCALL', 'RETURN' },
+        [2] = 'JMP',
+        [3] = { 'EQ', 'LT', 'LE', 'TEST', 'TESTSET' },
+        [4] = { 'FORLOOP', 'TFORLOOP' },
+        [5] = 'FORPREP',
+      }
+    return FlowOpcodes
+  end
+_G.package.preload['callgraph.vm_2015.get_next_ones'] =
+  function(...)
+    local get_next_ones
+    do
+      local Terminators_Map
+      local opcode_jmp
+      local BasicForks_Map
+      local Loopbacks_Map
+      local opcode_forprep
+      do
+        local FlowOpcodes = request('FlowOpcodes')
+        local map_values = request('!.table.map_values')
+        Terminators_Map = map_values(FlowOpcodes[1])
+        opcode_jmp = FlowOpcodes[2]
+        BasicForks_Map = map_values(FlowOpcodes[3])
+        Loopbacks_Map = map_values(FlowOpcodes[4])
+        opcode_forprep = FlowOpcodes[5]
+      end
+      local add_to_list = request('!.concepts.list.add_item')
+      get_next_ones =
+        function(instruction_index, Instruction)
+          local NextOnes = {}
+          local opcode = Instruction[1]
+          local next_instruction = instruction_index + 1
+          if Terminators_Map[opcode] then
+            ;
+          elseif (opcode == opcode_jmp) then
+            add_to_list(
+              NextOnes, next_instruction + tonumber(Instruction[3])
+            )
+          elseif BasicForks_Map[opcode] then
+            add_to_list(NextOnes, next_instruction)
+            add_to_list(NextOnes, next_instruction + 1)
+          elseif Loopbacks_Map[opcode] then
+            add_to_list(
+              NextOnes, next_instruction + tonumber(Instruction[3])
+            )
+            add_to_list(NextOnes, next_instruction)
+          elseif (opcode == opcode_forprep) then
+            add_to_list(
+              NextOnes, next_instruction + tonumber(Instruction[3])
+            )
+          else
+            add_to_list(NextOnes, next_instruction)
+          end
+          return NextOnes
+        end
+    end
+    return get_next_ones
   end
 _G.package.preload['callgraph.callgraph_to_dot.DotSerializer'] =
   function(...)
