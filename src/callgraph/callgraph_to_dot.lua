fb847c8c578292a8cf0d256d384aac7b353e81ac
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-15 04:57:26 +0200

    [^] [callgraph_to_dot] Code composition..
    [^<] Exported core to [TokensOutputStream]..
    
      This is design finding from [codec_lua_graph].
    
    [/] Prefixing names with "_" instead of quoting.
    
    [<] [callgraph.get_chains] Extracted from [callgraph_to_dot].

--- src/callgraph/callgraph_to_dot.lua
+++ src/callgraph/callgraph_to_dot.lua
@@ -21,64 +21,8 @@
   It has expressive syntax and nice for manual editing.
 ]]
 
---[[
-  This implementation merges chains into one .dot statement.
-]]
-
-local get_chains
-do
-  local add_to_list = request('!.concepts.list.add_item')
-
-  get_chains =
-    function(InstructionsGraph)
-      local num_instructions = #InstructionsGraph
-
-      local Chains = { }
-
-      local VisitedNodes_Map = { }
-      for i = 1, num_instructions do
-        VisitedNodes_Map[i] = false
-      end
-
-      local walk_chain
-      walk_chain =
-        function(start_node, Chain)
-          add_to_list(Chain, start_node)
-
-          if VisitedNodes_Map[start_node] then
-            add_to_list(Chains, Chain)
-            return
-          end
-
-          VisitedNodes_Map[start_node] = true
-
-          local NextOnes = InstructionsGraph[start_node].NextOnes
-          local num_next_ones = #NextOnes
-
-          if (num_next_ones == 0) then
-            add_to_list(Chains, Chain)
-            return
-          end
-
-          walk_chain(NextOnes[1], Chain)
-
-          for next_one_index = 2, num_next_ones do
-            local InnerChain = { start_node }
-            walk_chain(NextOnes[next_one_index], InnerChain)
-          end
-        end
-
-      for start_node = 1, num_instructions do
-        if not VisitedNodes_Map[start_node] then
-          walk_chain(start_node, { })
-        end
-      end
-
-      return Chains
-    end
-end
-
 local Writer = request('callgraph_to_dot.Writer')
+local get_chains = request('get_chains')
 
 -- Export:
 return
--- src/callgraph/callgraph_to_dot/Writer.lua
+++ src/callgraph/callgraph_to_dot/Writer.lua
@@ -2,121 +2,22 @@
 
 --[[
   Author: Martin Eden
-  Last mod.: 2026-09-14
+  Last mod.: 2026-09-15
 ]]
 
 --[[
   Internal state
 
-    1 [t] -- output stream instance
-    2 [i] -- current line length
-    3 [s] -- previous token
-    4 [t] -- indent instance
-    5 [t] -- padded index instance
+    1 [t] -- tokens writer instance
+    2 [t] -- padded index instance
 ]]
 
 local Syntels = request('Syntels')
-local Spaces = request('Spaces')
 
-local end_line
-local empty_line
-do
-  local line_separator = Spaces.newline
-  end_line =
-    function(Me)
-      if (Me[2] == 0) then return end
-
-      Me[1]:Write(line_separator)
-      Me[2] = 0
-      Me[3] = ''
-    end
-
-  empty_line =
-    function(Me)
-      end_line(Me)
-      Me[1]:Write(line_separator)
-    end
-end
-
-local write
-do
-  local item_separator = Spaces.space
-  local sep_len = #item_separator
-  local is_alnum = request('!.concepts.Ascii.is_alnum')
-  local str_sub = string.sub
-  local str_byte = string.byte
-  local end_statement = Syntels.end_statement
-  local ends_with = request('!.string.ends_with')
-  local wrapping_len = 53
-  local arrow = Syntels.arrow
-  write =
-    function(Me, token)
-      local OutputStream = Me[1]
-      local line_len = Me[2]
-      local prev_token = Me[3]
-
-      if (line_len == 0) then
-        OutputStream:Write(Me[4]:ToString())
-      end
-
-      if
-        (line_len > wrapping_len) and
-        (
-          (prev_token == arrow) or
-          (prev_token == end_statement)
-        )
-      then
-        end_line(Me)
-        local Indent = Me[4]
-        OutputStream:Write(Indent:ToString())
-        OutputStream:Write(Indent:GetIndentChunk())
-      else
-        local write_sep = false
-
-        if (prev_token ~= '') then
-          local prev_char_code = str_byte(str_sub(prev_token, -1, -1))
-          local next_char_code = str_byte(str_sub(token, 1, 1))
-          write_sep =
-            -- Separation is strictly needed say between "strict" and "digraph"
-            (is_alnum(prev_char_code) and is_alnum(next_char_code)) or
-            -- Opportunistically add separation to anything except ";" and " "
-            (
-              (token ~= end_statement) and
-              not ends_with(prev_token, item_separator)
-            )
-        end
-
-        if write_sep then
-          OutputStream:Write(item_separator)
-          Me[2] = Me[2] + sep_len
-        end
-      end
-
-      OutputStream:Write(token)
-
-      Me[2] = Me[2] + #token
-      Me[3] = token
-    end
-end
-
-local end_statement
-do
-  local end_statement_str = Syntels.end_statement
-  end_statement =
-    function(Me)
-      write(Me, end_statement_str)
-      end_line(Me)
-    end
-end
-
-local arrow
-do
-  local arrow_str = Syntels.arrow
-  arrow =
-    function(Me)
-      write(Me, arrow_str)
-    end
-end
+local EmptyLine =
+  function(Me)
+    Me[1]:EmptyLine()
+  end
 
 local quote
 do
@@ -127,41 +28,20 @@ do
     end
 end
 
-local get_node_name =
-  function(Me, index)
-    return quote(Me[5]:ToString(index))
-  end
-
-local label
-do
-  local start_attr = Syntels.start_attr
-  local end_attr = Syntels.end_attr
-  local label_kw = Syntels.kw_label
-  local assign = Syntels.assign
-  label =
-    function(Me, label)
-      write(Me, start_attr)
-      write(Me, label_kw)
-      write(Me, assign)
-      write(Me, quote(label))
-      write(Me, end_attr)
-    end
-end
-
 local StartGraph
 do
   local digraph = Syntels.kw_digraph
   local start_graph = Syntels.start_graph
   StartGraph =
     function(Me, graph_name)
-      write(Me, digraph)
+      local Tokens = Me[1]
+      Tokens:Write(digraph)
       if graph_name then
-        write(Me, quote(graph_name))
+        Tokens:Write(quote(graph_name))
       end
-      end_line(Me)
-      write(Me, start_graph)
-      end_line(Me)
-      Me[4]:Inc()
+      Tokens:EndLine()
+      Tokens:Write(start_graph)
+      Tokens:EndLine()
     end
 end
 
@@ -170,63 +50,77 @@ do
   local end_graph = Syntels.end_graph
   EndGraph =
     function(Me)
-      Me[4]:Dec()
-      end_line(Me)
-      write(Me, end_graph)
-      end_line(Me)
+      local Tokens = Me[1]
+      Tokens:EndLine()
+      Tokens:Write(end_graph)
+      Tokens:EndLine()
     end
 end
 
-local Node =
-  function(Me, index, label_str)
-    write(Me, get_node_name(Me, index))
-    label(Me, label_str)
-    end_statement(Me)
-  end
-
-local Chain =
-  function(Me, Chain)
-    local num_nodes = #Chain
-    if (num_nodes <= 1) then return end
-
-    local prev_node = Chain[1]
-
-    write(Me, get_node_name(Me, prev_node))
-
-    for node_idx = 2, num_nodes do
-      local next_node = Chain[node_idx]
-      arrow(Me)
-      write(Me, get_node_name(Me, next_node))
-      prev_node = next_node
+local get_node_name
+do
+  local name_prefix = '_'
+  get_node_name =
+    function(Me, index)
+      return name_prefix .. Me[2]:ToString(index)
     end
+end
 
-    end_statement(Me)
-  end
+local Node
+do
+  local start_attr = Syntels.start_attr
+  local end_attr = Syntels.end_attr
+  local label_kw = Syntels.kw_label
+  local assign = Syntels.assign
+  local end_statement = Syntels.end_statement
+  Node =
+    function(Me, index, label)
+      local Tokens = Me[1]
+      Tokens:Write(get_node_name(Me, index))
+      Tokens:Write(start_attr)
+      Tokens:Write(label_kw)
+      Tokens:Write(assign)
+      Tokens:Write(quote(label))
+      Tokens:Write(end_attr)
+      Tokens:Write(end_statement)
+      Tokens:EndLine()
+    end
+end
+
+local Chain
+do
+  local arrow = Syntels.arrow
+  local end_statement = Syntels.end_statement
+  Chain =
+    function(Me, Chain)
+      local Tokens = Me[1]
+
+      local num_nodes = #Chain
+      if (num_nodes <= 1) then return end
+
+      Tokens:Write(get_node_name(Me, Chain[1]))
+      for node_idx = 2, num_nodes do
+        Tokens:Write(arrow)
+        Tokens:Write(get_node_name(Me, Chain[node_idx]))
+      end
+      Tokens:Write(end_statement)
+      Tokens:EndLine()
+    end
+end
 
 local Methods
 do
   local create
   do
     local attach_methods = request('!.table.attach_methods')
-    local indent = '   '
-    local Indent = request('!.concepts.Indent')
+    local TokensWriter = request('TokensOutputStream')
     local IndexSerializer = request('!.concepts.PaddedIndex')
     create =
-      function(Arg_OutputStream, num_nodes)
-        OutputStream = Arg_OutputStream
-
-        Indent = Indent.create()
-        Indent:SetIndentChunk(indent)
-
-        IndexSerializer = IndexSerializer.create(num_nodes)
-
+      function(OutputStream, num_nodes)
         local Core =
           {
-            [1] = Arg_OutputStream,
-            [2] = 0,
-            [3] = '',
-            [4] = Indent,
-            [5] = IndexSerializer,
+            [1] = TokensWriter.create(OutputStream),
+            [2] = IndexSerializer.create(num_nodes),
           }
         attach_methods(Core, Methods)
 
@@ -238,7 +132,7 @@ do
     {
       create = create,
 
-      EmptyLine = empty_line,
+      EmptyLine = EmptyLine,
 
       StartGraph = StartGraph,
       EndGraph = EndGraph,
@@ -252,6 +146,6 @@ do
 return Methods
 
 --[[
-  2026 # # # # #
-  2026-09-14
+  2026 # # # # # #
+  2026-09-15
 ]]
