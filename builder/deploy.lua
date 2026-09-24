d739ecf6e934d5b49f0ebf2ac52e83bfd0c49a90
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-09-24 18:50:09 +0200

    [/~] [builder.create_deploy] Renamed to [deploy] and another impl.

---
+++ builder/deploy.lua
@@ -0,0 +1,58 @@
+-- Load modules, get list of all required Lua files, copy them
+
+--[[
+  Author: Martin Eden
+  Last mod.: 2026-09-24
+]]
+
+--[[
+  How to use
+
+  * Include root modules in <ModulesList>
+
+  * Do one of
+
+    * Copy this file to root Lua source directory
+
+    * Call this file from Lua source directory:
+
+        $ lua ../builder/create_deploy.lua
+
+  Make sure that main Lua file executes without errors when
+  loaded as module. If needed, make changes to it to behave so.
+]]
+
+--
+local Modules = { 'generate_callgraphs_lua' }
+--
+package.path = package.path .. ';../../../?.lua'
+--
+local ModulePaths
+do
+  arg[1] = '../samples/test.lua'
+  arg[2] = '../output/'
+  local observe_modules = require('workshop.system.observe_modules')
+  ModulePaths = observe_modules(Modules)
+end
+--
+require('workshop.base')
+
+local FilesList
+do
+  local add_to = request('!.concepts.list.add_item')
+  FilesList = { }
+  for _, ModuleLoc in ipairs(ModulePaths) do
+    add_to(FilesList, ModuleLoc[2])
+  end
+end
+
+local deploy = request('!.mechs.deploy')
+
+deploy(FilesList)
+--
+
+--[[
+  202?
+  2026 # # # #
+  2026-09-24
+]]
